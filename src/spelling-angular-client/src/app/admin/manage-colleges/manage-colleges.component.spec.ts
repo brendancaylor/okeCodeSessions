b354d7b4a18e13edbc6bc3ef815062141e540ac2
Brendan Caylor <brendan.caylor@gmail.com>
2019-12-05 01:07:56 +0000

    Add list of colleges in fe

--- src/spelling-angular-client/src/app/admin/manage-colleges/manage-colleges.component.spec.ts
+++ src/spelling-angular-client/src/app/admin/manage-colleges/manage-colleges.component.spec.ts
@@ -1,6 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {
+  MatButtonModule,
+  MatDialogModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSelectModule,
+  MatTableModule,
+  MatToolbarModule,
+  MatDialog,
+} from '@angular/material';
+
+import { BrowserModule } from '@angular/platform-browser';
 import { ManageCollegesComponent } from './manage-colleges.component';
+import { CoreModule } from 'src/app/core/core.module';
+import { CollegeClient } from 'src/app/core/services/clients';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('ManageCollegesComponent', () => {
   let component: ManageCollegesComponent;
@@ -8,7 +26,22 @@ describe('ManageCollegesComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ManageCollegesComponent ]
+      imports: [
+        HttpClientTestingModule,
+        BrowserModule,
+        FormsModule,
+        MatFormFieldModule,
+        MatDialogModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatTableModule,
+        MatInputModule,
+        MatSelectModule,
+        CoreModule,
+        RouterTestingModule
+      ],
+      declarations: [ ManageCollegesComponent ],
+      providers: [CollegeClient, MatDialog]
     })
     .compileComponents();
   }));
--- src/spelling-angular-client/src/app/admin/manage-colleges/manage-colleges.component.ts
+++ src/spelling-angular-client/src/app/admin/manage-colleges/manage-colleges.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource, MatDialog } from '@angular/material';
+import { CollegeClient, CollegeDto } from 'src/app/core/services/clients';
 
 @Component({
   selector: 'app-manage-colleges',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manage-colleges.component.scss']
 })
 export class ManageCollegesComponent implements OnInit {
+  displayedColumns = ['collegeName'];
+  dataSource = new MatTableDataSource<CollegeDto>();
+  colleges: Array<CollegeDto>;
 
-  constructor() { }
+  constructor(
+    private _collegeClient: CollegeClient,
+    public dialog: MatDialog
+  ) { }
 
   ngOnInit() {
+    this._collegeClient.getAllColleges()
+    .subscribe(
+      (data) => {
+        this.colleges = data;
+        this.dataSource.data = data;
+      }
+    );
   }
 
 }
